d0b8f2b94166bb6cb15ac2f2e23c9d5ebabaed67
Alexandra Țugurlan <93049817+atugurlan@users.noreply.github.com>
2023-05-08 21:53:54 +0300

    simple registration: signup + login + logout

---
+++ src/main/java/com/example/sef_project/SignUpController.java
@@ -0,0 +1,63 @@
+package com.example.sef_project;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SignUpController implements Initializable {
+    @FXML
+    private Button button_signup;
+
+    @FXML
+    private Button button_log_in;
+
+    @FXML
+    private RadioButton rb_therapist;
+
+    @FXML
+    private RadioButton rb_pacient;
+
+    @FXML
+    private TextField tf_username;
+
+    @FXML
+    private TextField tf_password;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        ToggleGroup toggleGroup = new ToggleGroup();
+        rb_therapist.setToggleGroup(toggleGroup);
+        rb_pacient.setToggleGroup(toggleGroup);
+
+        rb_therapist.setSelected(true);
+
+        button_signup.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                String toggleName = ((RadioButton) toggleGroup.getSelectedToggle()).getText();
+
+                if(!tf_username.getText().trim().isEmpty() && !tf_password.getText().trim().isEmpty()) {
+                    DBUtils.signUpUser(event, tf_username.getText(), tf_password.getText(), toggleName);
+                }
+                else {
+                    System.out.println("Please fill in all the information!");
+                    Alert alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setContentText("Please fill in all the information to sign up!");
+                    alert.show();
+                }
+            }
+        });
+
+        button_log_in.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                DBUtils.changeScene(event, "sample.fxml", "Log In!", null, null);
+            }
+        });
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.sef_project {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens com.example.sef_project to javafx.fxml;
