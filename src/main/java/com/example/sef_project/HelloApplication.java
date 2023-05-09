8aaaf43f955446c60297026698c7482a57af06b6
Cristina <cristina.rodean123@gamil.com>
2023-05-09 22:31:54 +0300

    designed sample and sign up

--- src/main/java/com/example/sef_project/HelloApplication.java
+++ src/main/java/com/example/sef_project/HelloApplication.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class  HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("sample.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
-        stage.setTitle("Application");
+        stage.setTitle("Therapy App");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.sef_project {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.example.sef_project to javafx.fxml;
-    exports com.example.sef_project;
-}
