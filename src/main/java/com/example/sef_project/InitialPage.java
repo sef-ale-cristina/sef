30158390bb67074aea525b27b5a4b9da918c9ded
Alexandra Țugurlan <93049817+atugurlan@users.noreply.github.com>
2023-05-15 14:55:23 +0300

    added the controller for the initial setup

--- src/main/java/com/example/sef_project/InitialPage.java
+++ src/main/java/com/example/sef_project/InitialPage.java
@@ -1,5 +1,34 @@
 package com.example.sef_project;
 
-public class InitialPage {
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class InitialPage implements Initializable {
+    @FXML
+    private Button b_signup;
+
+    @FXML
+    private Button b_login;
+
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        b_login.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                DBUtils.changeScene(event, "log-in.fxml", "Log In!", null, null);
+            }
+        });
+
+        b_signup.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                DBUtils.changeScene(event, "sign-up.fxml", "Sign Up!", null, null);
+            }
+        });
+    }
 }
