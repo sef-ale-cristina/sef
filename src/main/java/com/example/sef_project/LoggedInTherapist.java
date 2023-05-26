7b03b684f7f05093d995a640a74945cd8efc7651
atugurlan <alexandra.tugurlan@student.upt.ro>
2023-05-26 19:58:54 +0300

    created the log out button for therapist

--- src/main/java/com/example/sef_project/LoggedInTherapist.java
+++ src/main/java/com/example/sef_project/LoggedInTherapist.java
@@ -1,15 +1,33 @@
 package com.example.sef_project;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class LoggedInTherapist {
+public class LoggedInTherapist implements Initializable {
     @FXML
     private Label l_welcome;
 
+    @FXML
+    private Button b_logout;
+
     public void setUserInformation(String username, String app_role) throws IOException {
         l_welcome.setText("Welcome, " + username + "!");
+    }
+
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        b_logout.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                DBUtils.changeScene(event, "log-in.fxml", "Log In!", null, null);
+            }
+        });
     }
 }
