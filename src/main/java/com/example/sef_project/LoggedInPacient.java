4f11f75e41bd8aa2062e39076c9803f9c234a2bd
atugurlan <alexandra.tugurlan@student.upt.ro>
2023-05-27 12:26:34 +0300

    started working on a create appointment feature for a pacient

--- src/main/java/com/example/sef_project/LoggedInPacient.java
+++ src/main/java/com/example/sef_project/LoggedInPacient.java
@@ -1,5 +1,7 @@
 package com.example.sef_project;
 
+import java.time.LocalDate;
+
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -8,6 +10,7 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.control.DatePicker;
 
 import java.io.IOException;
 import java.net.URL;
@@ -15,8 +18,13 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Locale;
 import java.util.ResourceBundle;
 import javafx.scene.control.TableView;
+
+import static com.mchange.v2.codegen.bean.BeangenUtils.capitalize;
 
 public class LoggedInPacient implements Initializable {
     @FXML
@@ -30,7 +38,10 @@ public class LoggedInPacient implements Initializable {
     private DatePicker date_appointment;
 
     @FXML
-    private ComboBox<String> cb_time;
+    private ComboBox<String> cb_time_begin;
+
+    @FXML
+    private ComboBox<String> cb_time_end;
 
     @FXML
     private ComboBox<String> cb_therapist;
@@ -49,6 +60,13 @@ public class LoggedInPacient implements Initializable {
 
     ObservableList<Therapist> listview = FXCollections.observableArrayList();
 
+    ArrayList<String> therapists = new ArrayList<String>();
+
+    String days = new String("");
+    String begin_time = new String("");
+    String end_time = new String("");
+
+    private Patient pacient = new Patient(null, null, null);
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         b_logout.setOnAction(new EventHandler<ActionEvent>() {
@@ -64,14 +82,17 @@ public class LoggedInPacient implements Initializable {
             DBConnect cn = new DBConnect();
             Connection cn1 = cn.getConnection();
 
-            String sql = "SELECT * FROM users WHERE app_role = 'therapist'";
+            String sql = "SELECT * FROM therapist";
             Statement s = cn1.createStatement();
             ResultSet r = s.executeQuery(sql);
 
             while(r.next()) {
+                String username = r.getString("username");
                 listview.add(new Therapist(
-                        r.getString("username")
+                        username
                 ));
+
+                therapists.add(username);
             }
 
             table_therapists.setItems(listview);
@@ -79,17 +100,97 @@ public class LoggedInPacient implements Initializable {
             throw new RuntimeException(e);
         }
 
-//        table_therapists.getItems().addAll(therapist);
-//        table_therapists.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
-//            @Override
-//            public void changed(ObservableValue<? extends String> observableValue, String s, String t1) {
-//                currentTherapist=table_therapists.getSelectionModel().getSelectedItem();
-//                selected_th.setText(currentTherapist);
-//            }
-//        });
+        Collections.sort(therapists);
+        cb_therapist.getItems().addAll(therapists);
+
+        cb_therapist.setOnAction(comboBoxTherapistEvent -> {
+            try {
+                DBConnect cn = new DBConnect();
+                Connection cn1 = cn.getConnection();
+
+                String username = cb_therapist.getValue();
+
+                String sql = "SELECT * FROM therapist WHERE username = '" + username + "'";
+                Statement s = cn1.createStatement();
+                ResultSet r = s.executeQuery(sql);
+
+                while(r.next()) {
+                    days = days.concat(r.getString("days"));
+                    begin_time = begin_time.concat(r.getString("start_time"));
+                    end_time = end_time.concat(r.getString("end_time"));
+                }
+                System.out.println(days);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+
+            date_appointment.setOnAction(datePickerEvent -> {
+                LocalDate localDate = date_appointment.getValue();
+                String week_day = localDate.getDayOfWeek().toString();
+                week_day = week_day.toLowerCase();
+                week_day = capitalize(week_day);
+                //System.out.println(week_day);
+
+                System.out.println(days.contains(week_day));
+                if(days.contains(week_day)==false) {
+                    System.out.println("The day you want to enter is outside the working program of the therapist!");
+                    Alert alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setContentText("The day you want to enter is outside the working program of the therapist!");
+                    alert.show();
+                }
+            });
+
+            cb_time_begin.getItems().addAll("08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00");
+
+            cb_time_end.getItems().addAll("09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00");
+
+            cb_time_begin.setOnAction(timeBeginEvent -> {
+                cb_time_end.setOnAction(timeEndEvent -> {
+                    String begin = cb_time_begin.getValue();
+                    String end = cb_time_end.getValue();
+
+                    if(begin.compareTo(end)==1) {
+                        System.out.println("Insert correct times for an appointment!");
+                        Alert alert = new Alert(Alert.AlertType.ERROR);
+                        alert.setContentText("Insert correct times for an appointment!");
+                        alert.show();
+                    }
+
+                    if(begin.compareTo(begin_time)==-1 || begin.compareTo(end_time)==1 || end.compareTo(begin_time)==-1 || end.compareTo(end_time)==1) {
+                        System.out.println("The time you selected is outside of the therapists's working hours!");
+                        Alert alert = new Alert(Alert.AlertType.ERROR);
+                        alert.setContentText("The time you selected is outside of the therapists's working hours!");
+                        alert.show();
+                    }
+
+                    b_schedule.setOnAction(scheduleEvent -> {
+                        try {
+                            DBConnect cn = new DBConnect();
+                            Connection cn1 = cn.getConnection();
+
+                            String username = cb_therapist.getValue();
+
+                            String sql = "SELECT * FROM appointment WHERE username = '" + username + "'";
+                            Statement s = cn1.createStatement();
+                            ResultSet r = s.executeQuery(sql);
+
+                            while(r.next()) {
+                                days = days.concat(r.getString("days"));
+                                begin_time = begin_time.concat(r.getString("start_time"));
+                                end_time = end_time.concat(r.getString("end_time"));
+                            }
+                            System.out.println(days);
+                        } catch (SQLException e) {
+                            throw new RuntimeException(e);
+                        }
+                    });
+                });
+            });
+        });
     }
 
     public void setUserInformation(String username, String app_role) throws IOException {
         label_welcome.setText("Welcome, " + username + "!");
+        pacient.setUsername(username);
     }
 }
